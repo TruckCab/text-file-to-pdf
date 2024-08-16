01c5c82294475943d58458b4e1fc706910979b7a
Philip Moore <prmoore77@hotmail.com>
2024-08-16 11:35:26 -0400

    WiP

--- text_file_to_pdf.sh
+++ text_file_to_pdf.sh
@@ -14,7 +14,7 @@ SCRIPT_USER=`whoami`
 LOG_DIR="${PGM_DIR}/logs"
 LOG_FILE=${LOG_DIR}/${PGM}_${PID}_`date +%m%d%y_%H%M%S`.log
 
-echo -e "Script: ${0} was called with arguments: ${1} ${2} ${3} ${4} ${5} ${6} ${7}" | tee -a ${LOG_FILE}
+echo -e "Script: ${0} - being run by: $(whoami) - was called with arguments: ${1} ${2} ${3} ${4} ${5} ${6} ${7}" | tee -a ${LOG_FILE}
 
 cd ${PGM_DIR}
 source .venv/bin/activate
